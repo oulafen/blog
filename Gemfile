33e4e1b544a0917810542498d02fd48d291fd55e
xiaofen <oulafen@gmail.com>
2014-03-07 16:07:34 +0800

    Use scaffold to create Post resource

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 ruby '2.0.0'
+gem 'haml'
 
 gem 'rails', '4.0.3'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Blog::Application.routes.draw do
+  resources :posts
+
+  root :to=>'home#index'
+
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
