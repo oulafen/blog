7b7603598b7eb0302fdc11e765a311966e649021
xiaofen <oulafen@gmail.com>
2014-03-08 17:47:02 +0800

    reinit the blog

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(:default, Rails.env)
 
 module Blog
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307080557) do
-
-  create_table "posts", force: true do |t|
-    t.string   "name"
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
