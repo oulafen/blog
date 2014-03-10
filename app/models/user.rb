7473a8f57ce0bf273370b20a95ca63254c5cd16b
xiaofen <oulafen@gmail.com>
2014-03-10 16:56:20 +0800

    Achieve a login authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
 
   attr_accessible :email, :name, :password, :password_confirmation, :token
-  before_create{generate_token(:token)}
 
-  def generate_token(column)
-    begin
-      self[column]=SecureRandom.urlsafe_base64
-    end while User.exists?(column=>self[column])
-  end
 end
